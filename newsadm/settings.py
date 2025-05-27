2a8dc8ce9e541f42c74ade72bc8c5b083a0b378c
41118114 <148747222+41118114@users.noreply.github.com>
2025-05-27 15:43:57 +0800

    Update settings.py

--- newsadm/settings.py
+++ newsadm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(d)=n()^=zc4fwff&m)24s)_mpffn#n+hbbtx@(g=2$zxog44%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
